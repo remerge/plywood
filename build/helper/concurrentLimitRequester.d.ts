419b7e93ebeb80cb600f9ec6182da58e6a931062
aiwanaszko <47663027+aiwanaszko@users.noreply.github.com>
2024-12-04 08:36:12 +0100

    remove obsolete prom-client import

--- build/helper/concurrentLimitRequester.d.ts
+++ build/helper/concurrentLimitRequester.d.ts
@@ -1,8 +1,12 @@
 import { PlywoodRequester } from 'plywood-base-api';
-import { Gauge } from 'prom-client';
+interface ConcurrentRequestStore {
+  inc(): void;
+  dec(): void;
+  requests(): number;
+}
 export interface ConcurrentLimitRequesterParameters<T> {
     requester: PlywoodRequester<T>;
     concurrentLimit: int;
-    concurrentRequests: Gauge;
+    concurrentRequests: ConcurrentRequestStore;
 }
 export declare function concurrentLimitRequesterFactory<T>(parameters: ConcurrentLimitRequesterParameters<T>): PlywoodRequester<T>;
